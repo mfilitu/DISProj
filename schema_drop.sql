c080ab8434204f66c1c98d210a1e5b7ee4d55b55
Kristoffer Sandvang <kristoffersandvang@gmail.com>
2023-05-31 13:09:25 +0200

    Added dropping of tables

---
+++ schema_drop.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS Rockets;
+DROP TABLE IF EXISTS Companies;
+DROP TABLE IF EXISTS Locations;
+DROP TABLE IF EXISTS Missions;
+DROP TABLE IF EXISTS Company_success_rate;
+DROP TABLE IF EXISTS Location_success_rate;
+DROP TABLE IF EXISTS Rocket_success_rate;
