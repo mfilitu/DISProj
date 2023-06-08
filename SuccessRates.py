df0c5f7bc32a73d223a6a5a4e30ba02a0c44d769
Kristoffer Sandvang <kristoffersandvang@gmail.com>
2023-06-08 12:39:27 +0200

    Added functionality for inserting new missions

--- SuccessRates.py
+++ SuccessRates.py
@@ -1,7 +1,16 @@
 import psycopg2
 conn = psycopg2.connect("dbname=distest user=postgres password=postgres")
 
-def Insert_RocketSuccesRate(conn): 
+def Insert_RocketSuccesRate(conn):
+    """
+    Calulates and inserts the success rate of each rocket in the database.
+
+    Args:
+        conn (connection): The connection with the database.
+
+    Returns:
+        None
+    """
     RID_cursor = conn.cursor()
     MID_cursor = conn.cursor()
     INS_cursor = conn.cursor()
@@ -10,11 +19,9 @@ def Insert_RocketSuccesRate(conn):
     for Rocket in Rocket_rows:
         rid = Rocket[0]
         MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE rocket_id = %s AND succesful = TRUE", (rid,))
-        successful_mission_rows = MID_cursor.fetchall()
-        successful = successful_mission_rows[0][0]
+        successful = MID_cursor.fetchone()[0]
         MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE rocket_id = %s", (rid,))
-        total_mission_rows = MID_cursor.fetchall()
-        total = total_mission_rows[0][0]
+        total = MID_cursor.fetchone()[0]
         sql = None
         if (total != 0):
             RocketSuccesRate = successful / total
@@ -23,9 +30,19 @@ def Insert_RocketSuccesRate(conn):
             sql = "INSERT INTO Rocket_success_rate (rocket_id, success_rate)VALUES (%s, NULL);".format(rid)
         INS_cursor.execute(sql)
         conn.commit()
+    return None
 
 
-def Insert_CompanySuccesRate(conn): 
+def Insert_CompanySuccesRate(conn):
+    """
+    Calulates and inserts the success rate of each company in the database.
+
+    Args:
+        conn (connection): The connection with the database.
+
+    Returns:
+        None
+    """
     CID_cursor = conn.cursor()
     MID_cursor = conn.cursor()
     INS_cursor = conn.cursor()
@@ -34,11 +51,9 @@ def Insert_CompanySuccesRate(conn):
     for company in Companies:
         cid = company[0]
         MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE company_id = %s AND succesful = TRUE", (cid,))
-        successful_mission_rows = MID_cursor.fetchone()
-        successful = successful_mission_rows[0]
+        successful = MID_cursor.fetchone()[0]
         MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE company_id = %s", (cid,))
-        total_mission_rows = MID_cursor.fetchone()
-        total = total_mission_rows[0]
+        total = MID_cursor.fetchone()[0]
         sql = None
         if (total == 0):
             sql = "INSERT INTO Company_success_rate (company_id, success_rate) VALUES ({}, NULL);".format(cid)
@@ -47,21 +62,31 @@ def Insert_CompanySuccesRate(conn):
             sql = "INSERT INTO Company_success_rate (company_id, success_rate) VALUES ({}, {});".format(cid, CompanySuccesRate)
         INS_cursor.execute(sql)
         conn.commit()
+    return None
 
-def Insert_LocationSuccesRate(conn): 
+def Insert_LocationSuccesRate(conn):
+    """
+    Calulates and inserts the success rate of each location in the database.
+
+    Args:
+        conn (connection): The connection with the database.
+
+    Returns:
+        None
+    """
     LID_cursor = conn.cursor()
     MID_cursor = conn.cursor()
     INS_cursor = conn.cursor()
     LID_cursor.execute("SELECT id FROM Locations")
     Locations = LID_cursor.fetchall()
+
     for location in Locations:
         lid = location[0]
         MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE location_id = %s AND succesful = TRUE", (lid,))
-        successful_mission_rows = MID_cursor.fetchone()
-        successful = successful_mission_rows[0]
+        successful = MID_cursor.fetchone()[0]
         MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE location_id = %s", (lid,))
-        total_mission_rows = MID_cursor.fetchone()
-        total = total_mission_rows[0]
+        total = MID_cursor.fetchone()[0]
+
         sql = None
         if (total == 0):
             sql = "INSERT INTO Location_success_rate (location_id, success_rate) VALUES ({}, NULL);".format(lid)
@@ -71,22 +96,159 @@ def Insert_LocationSuccesRate(conn):
         INS_cursor.execute(sql)
         conn.commit()
 
-# def Update_RocketSuccesRate(conn, rocket_id): 
-#     MID_cursor = conn.cursor()
-#     INS_cursor = conn.cursor()
-#     MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE rocket_id = %s AND succesful = TRUE", (rocket_id,))
-#     successful_mission_rows = MID_cursor.fetchone()
-#     successful = successful_mission_rows[0]
-#     MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE rocket_id = %s", (rocket_id,))
-#     total_mission_rows = MID_cursor.fetchone()
-#     total = total_mission_rows[0]
+def Update_RocketSuccesRate(conn, rocket_id): 
+    """
+    Calulates and updates the success rate of each location in the database.
 
+    Args:
+        conn (connection): The connection with the database.
+        rocket_id (integer): the ID of the rocket which success rate needs updated. 
 
+    Returns:
+        None
+    """
+    MID_cursor = conn.cursor()
+    INS_cursor = conn.cursor()
 
+    MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE rocket_id = %s AND succesful = TRUE", (rocket_id,))
+    successful_mission_rows = MID_cursor.fetchone()
+    successful = successful_mission_rows[0]
+    MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE rocket_id = %s", (rocket_id,))
+    total_mission_rows = MID_cursor.fetchone()
+    total = total_mission_rows[0]
+    RocketSuccesRate = successful / total
+    
+    INS_cursor.execute("""UPDATE Rocket_success_rate
+                          SET success_rate = %s
+                          WHERE rocket_id = %s;""", (RocketSuccesRate, rocket_id))
+    conn.commit()
+    return None
+
+def Update_RocketSuccesRate(conn, rocket_id): 
+    """
+    Calulates and updates the success rate of each location in the database.
+
+    Args:
+        conn (connection): The connection with the database.
+        rocket_id (integer): the ID of the rocket which success rate needs updating. 
+
+    Returns:
+        None
+    """
+    MID_cursor = conn.cursor()
+    INS_cursor = conn.cursor()
+
+    MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE rocket_id = %s AND succesful = TRUE", (rocket_id,))
+    successful_mission_rows = MID_cursor.fetchone()
+    successful = successful_mission_rows[0]
+    MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE rocket_id = %s", (rocket_id,))
+    total_mission_rows = MID_cursor.fetchone()
+    total = total_mission_rows[0]
+    RocketSuccesRate = successful / total
+    
+    INS_cursor.execute("""UPDATE Rocket_success_rate
+                          SET success_rate = %s
+                          WHERE rocket_id = %s;""", (RocketSuccesRate, rocket_id))
+    conn.commit()
+    return None
+
+def Update_CompanySuccesRate(conn, company_id): 
+    """
+    Calulates and updates the success rate of each location in the database.
+
+    Args:
+        conn (connection): The connection with the database.
+        company_id (integer): the ID of the company which success rate needs updating. 
+
+    Returns:
+        None
+    """
+    MID_cursor = conn.cursor()
+    INS_cursor = conn.cursor()
+
+    MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE company_id = %s AND succesful = TRUE", (company_id,))
+    successful_mission_rows = MID_cursor.fetchone()
+    successful = successful_mission_rows[0]
+    MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE company_id = %s", (company_id,))
+    total_mission_rows = MID_cursor.fetchone()
+    total = total_mission_rows[0]
+    SuccesRate = successful / total
+    
+    INS_cursor.execute("""UPDATE Company_success_rate
+                          SET success_rate = %s
+                          WHERE company_id = %s;""", (SuccesRate, company_id))
+    conn.commit()
+    return None
+
+def Update_LocationSuccesRate(conn, location_id): 
+    """
+    Calulates and updates the success rate of each location in the database.
+
+    Args:
+        conn (connection): The connect with the database.
+        location_id (integer): the ID of the location which success rate needs updating. 
+
+    Returns:
+        None
+    """
+    MID_cursor = conn.cursor()
+    INS_cursor = conn.cursor()
+
+    MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE location_id = %s AND succesful = TRUE", (location_id,))
+    successful_mission_rows = MID_cursor.fetchone()
+    successful = successful_mission_rows[0]
+    MID_cursor.execute("SELECT COUNT(succesful) FROM Missions WHERE location_id = %s", (location_id,))
+    total_mission_rows = MID_cursor.fetchone()
+    total = total_mission_rows[0]
+    SuccesRate = successful / total
+    
+    INS_cursor.execute("""UPDATE Company_success_rate
+                          SET success_rate = %s
+                          WHERE company_id = %s;""", (SuccesRate, location_id))
+    conn.commit()
+    return None
+    
 
 def Insert_SuccesRates(conn):
-    Insert_RocketSuccesRate(conn)
-    Insert_CompanySuccesRate(conn)
-    Insert_LocationSuccesRate(conn)
+    """
+    Calulates and inserts the success rate of each rocket, company,
+    and location in the database.
 
-Insert_SuccesRates(conn)
+    Args:
+        conn (connection): The connection with the database.
+
+    Returns:
+        None
+    """
+    Insert_RocketSuccesRate(conn)
+    print("Inserted Rocket succes rate")
+    Insert_CompanySuccesRate(conn)
+    print("Inserted company succes rate")
+    Insert_LocationSuccesRate(conn)
+    print("Inserted location succes rate")
+    return None
+
+def Update_SuccesRates(conn, mission_id):
+    """
+    Calulates and updates the success rate of each rocket, company,
+    and location in the database.
+
+    Args:
+        conn (connection): The connection with the database.
+        mission_id (integer): the ID of the newly inserted mission. 
+
+    Returns:
+        None
+    """
+    cursor = conn.cursor()
+    cursor.execute("""SELECT rocket_id, company_id, location_id 
+                      FROM Missions
+                      WHERE id = %s""", (mission_id,))
+    rid, cid, lid = cursor.fetchone()
+    Update_RocketSuccesRate(conn, rid)
+    print("Updatede rocket succes rate")
+    Update_CompanySuccesRate(conn, cid)
+    print("Updatede company succes rate")
+    Update_LocationSuccesRate(conn, lid)
+    print("Updatede location succes rate")
+    return None
