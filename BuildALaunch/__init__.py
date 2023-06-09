f1d543f7181a7d2bb36ad0f67833946986e68f14
mfilitu <70143907+mfilitu@users.noreply.github.com>
2023-06-09 19:08:39 +0200

    Changes to site

--- BuildALaunch/__init__.py
+++ BuildALaunch/__init__.py
@@ -19,10 +19,9 @@ conn = psycopg2.connect(db)
 def home():
     return render_template('home.html')
 
-@app.route('/companies')
+@app.route('/companies', methods=['POST', 'GET'])
 def companies():
 	companies = get_company_name(conn)
-	
 	return render_template('companies.html', companies=companies)
 
 @app.route('/rockets', methods=['POST', 'GET'])
@@ -33,8 +32,25 @@ def rockets():
 
 @app.route('/locations', methods=['POST', 'GET'])
 def location():
-	locations = get_location(conn)
+	locations = get_locations(conn)
 	return render_template('locations.html', locations=locations)
+
+@app.route('/countries', methods=['POST', 'GET'])
+def countries():
+	countries = get_country(conn)
+	if request.method == 'POST':
+		selected_country = request.form.get('country')
+		return render_template('countries.html', countries=countries, country1=selected_country)
+	return render_template('countries.html', countries=countries)
+
+@app.route('/launch', methods=['POST', 'GET'])
+def launch():
+    country = request.form.get('country')
+    rocket = request.form.get('rocket')
+    company = request.form.get('company')
+    location = request.form.get('location')
+    
+    return render_template('launch.html', country=country, rocket=rocket, company=company, location=location)
 
 
 def get_company_name(conn):
@@ -49,9 +65,14 @@ def get_rockets_by_company(conn):
 	rockets = cur.fetchall()
 	return rockets
 
-def get_location(conn):
+def get_locations(conn):
 	cur = conn.cursor()
 	cur.execute("SELECT location_name FROM Locations;")
 	locations = cur.fetchall()
 	return locations
 
+def get_country(conn):
+	cur = conn.cursor()
+	cur.execute("SELECT country FROM Locations;")
+	countries = cur.fetchall()
+	return countries
