a1d26d29f02a178180ac4ce9e5ba3fa68cacf13b
mfilitu <70143907+mfilitu@users.noreply.github.com>
2023-06-10 15:14:51 +0200

    Added documentation and updated html pages

--- BuildALaunch/__init__.py
+++ BuildALaunch/__init__.py
@@ -4,27 +4,34 @@ import psycopg2
 
 #psql -U postgres -d distest -f .\schema.sql
 #psql -U postgres -d distest -f .\schema_ins.sql
+
 app = Flask(__name__, template_folder='templates', static_folder='templates/css')
 app.config['SECRET_KEY'] = 'secret'
 
 # set your own database
-#db = "dbname='bank' user='postgres' host='127.0.0.1' password = 'UIS'"
+#db = "dbname='' user='' host='' password = ''"
 db = "dbname=distest user=postgres password=1074"
 conn = psycopg2.connect(db)
 
 # Inserts the succesrates of all rockets, companies and location in the db
 # Insert_SuccesRates(conn)
 
+# Home page of the website
+# app.route is a decorator that tells the application which URL should call the associated function
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# Companies page of the website where the user can choose a company
 @app.route('/companies', methods=['POST', 'GET'])
 def companies():
 	companies = get_company_info(conn)
 	
+	# render_template() is a function that takes a template filename and a variable list of template
+	# arguments and returns the same template, but with all the placeholders in it replaced with actual values
 	return render_template('companies.html', companies=companies)
 
+# Rockets page of the website where the user can choose a rocket
 @app.route('/rockets', methods=['POST', 'GET'])
 def rockets():
 	cid = request.args.get('cid')
@@ -32,15 +39,17 @@ def rockets():
 	companies = get_companies_id(conn)
 	return render_template('rockets.html', rocketInfo=rocketInfo, cid=cid, companies=companies)
 
+# Locations page of the website where the user can choose a location
 @app.route('/locations', methods=['POST', 'GET'])
 def location():
 	cid = request.args.get('cid')
 	rid = request.args.get('rid')
 	in_company = request.args.get('rocket')
 	locations = get_location(conn)
-	return render_template('locations.html', locations=locations, cid=cid, rid=rid)
+	countries = get_country(conn)
+	return render_template('locations.html', locations=locations, countries=countries, cid=cid, rid=rid)
 
-
+# Countries page of the website where the user can choose a country
 @app.route('/countries', methods=['POST', 'GET'])
 def countries():
 	countries = get_country(conn)
@@ -49,6 +58,7 @@ def countries():
 		return render_template('countries.html', countries=countries, country1=selected_country)
 	return render_template('countries.html', countries=countries)
 
+# Launch page of the website where the user can see the launch information and the success rate
 @app.route('/launch', methods=['POST', 'GET'])
 def launch():
     country = request.form.get('country')
@@ -58,7 +68,9 @@ def launch():
     
     return render_template('launch.html', country=country, rocket=rocket, company=company, location=location)
 
-
+# This code gets the company name, the number of missions the company has, the company success rate,
+# and the company id from the companies table, missions table, and company_success_rate table.
+#  It then stores the results of the query in a variable called companies.
 def get_company_info(conn):
 	cur = conn.cursor()
 	cur.execute("""SELECT companies.company_name, COUNT(*) AS mission_count,
@@ -71,11 +83,15 @@ def get_company_info(conn):
 	companies = cur.fetchall()
 	return companies
 
+# This code gets a list of all the companies in the database, 
+# and creates a list of all the company names and their ids.
 def get_companies_id(conn):
 	cur = conn.cursor()
 	cur.execute("""SELECT company_name, id FROM companies""")
 	return cur.fetchall()
 
+
+# This code gets all the rocket information from the database and returns it in a list of tuples
 def get_rocket_info(conn):
 	cur = conn.cursor()
 	cur.execute("""SELECT companies.id, rocket_name, success_rate, rockets.id FROM Rockets
@@ -86,12 +102,14 @@ def get_rocket_info(conn):
 	rockets = cur.fetchall()
 	return rockets
 
+# This code gets all the location names from the database and returns them as a list. 
 def get_location(conn):
 	cur = conn.cursor()
 	cur.execute("SELECT location_name FROM Locations;")
 	locations = cur.fetchall()
 	return locations
 
+# This function returns all distinct countries in the Locations table.
 def get_country(conn):
 	cur = conn.cursor()
 	cur.execute("SELECT DISTINCT country FROM Locations;")
